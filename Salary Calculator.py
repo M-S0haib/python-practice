9051804cfb7f0e8e594e796cf7a6d9bfbadecccf
M.Sohaib <146997582+M-S0haib@users.noreply.github.com>
2024-03-12 17:25:55 +0000

    Done

--- Salary Calculator.py
+++ Salary Calculator.py
@@ -1,1 +1,32 @@
+print("**----SALARY CALCULATOR----**")
+
+print("ENTER YOUR NAME")
+name = input()
+
+print("ENTER YOUR EMPLOYEE NUMBER")
+empNum = int(input())
+
+print("ENTER YOUR SALARY")
+salary = int(input())
+
+if(salary >= 50000):
+    tax = (salary * 15) / 100
+    salary1 = salary - tax
+
+elif(salary>=40000):
+    tax = (salary * 10) / 100
+    salary1 = salary - tax
+
+elif(salary>=30000):
+    tax = (salary * 5) / 100
+    salary1 = salary - tax
+
+else:
+    salary1 = salary
+
+allowance = (salary1 * 0.5)  / 100    
+
+finalSalary = allowance + salary1
+
+print("YOU GOT A MOBILE ALLOWANCE & YOUR SALARY IS : " , finalSalary)
 
