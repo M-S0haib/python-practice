1f2af4cc56b08b219b08933d76e409f210765583
M.Sohaib <146997582+M-S0haib@users.noreply.github.com>
2024-03-15 15:33:40 +0500

    Create Ride Booking.py

---
+++ Ride Booking.py
@@ -0,0 +1,31 @@
+print("welcome to RIDE BOOKING")
+
+print("Sign UP or Login")
+
+print("Enter your cell no / Email address")
+info = input()
+
+print("Enter your password")
+password = input()
+
+print("You have signed Up successfully")
+
+print("we have 4 categories of vehicles")
+
+print("select the suitable ride")
+
+print("BIKE AUTO MiniCAR AcCAR ")
+
+ride = str(input())
+
+print(" ENTER YOUR PICKUP LOCATION")
+pickUp = str(input())
+
+print(" Select the DropOff")
+
+print("lasbella Nazimabad FiveStar Nagan NorthKarachi")
+
+dropOff = str(input())
+
+location = " Lasbella" ,  "Nazimabad" , "FiveStar" , "Nagan" , "NorthKarachi"
+
